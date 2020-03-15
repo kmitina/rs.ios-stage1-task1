3e54f621c465b9b9b0bc349e687612b17109aa95
kris <kristmitina@gmail.com>
2020-03-16 00:21:46 +0300

    Completed 'T1Array', 'MiniMaxArrayConverter', 'BillCounter'

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,26 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+     if (sadArray.count == 0) {
+           return sadArray;
+       }
+
+       NSMutableArray* result = [[NSMutableArray alloc] initWithArray:sadArray];
+           for (int i = 1; i < result.count - 1; i++) {
+               int currentIndexValue = [result[i] intValue];
+               int sum = [result[i-1] intValue] + [result[i+1] intValue];
+
+               if (currentIndexValue > sum) {
+                   [result removeObjectAtIndex: i];
+                   i--;
+
+                   if (i > 1) {
+                       i--;
+                   }
+               }
+           }
+           return (NSArray*)result;
+    
 }
 
 @end
--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,16 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+
+    NSMutableArray *sumWithoutAnnDish = [[NSMutableArray alloc] initWithArray: bill];
+    [sumWithoutAnnDish removeObjectAtIndex:index];
+    int annSum = [[sumWithoutAnnDish valueForKeyPath:@"@sum.self"] intValue] / 2;
+
+    if ([sum intValue] == annSum) {
+        return  @"Bon Appetit";
+    } else {
+        return [NSString stringWithFormat:@"%d", [sum intValue] - annSum];
+    }
 }
 
 @end
